e7f3831d51a402142cb8732824d9f860ac80c7ac
chamc <chamchi0129@gmail.com>
2024-03-04 16:57:11 +0900

    Setup essential code

--- cogs/testcog.py
+++ cogs/testcog.py
@@ -8,11 +8,15 @@ class TestCog(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         await self.bot.tree.sync()
-        print(f'{__name__} cog is ready')
+        print(f'{__name__} is ready')
 
-    @commands.hybrid_command(name='핑', description='봇이 대답합니다.')
+    @commands.hybrid_command(name='핑', description='봇이 대답 합니다.')
     async def ping(self, ctx):
         await ctx.reply('퐁!')
+
+    @commands.hybrid_command(name='대답', description='봇이 똑같이 말 합니다.')
+    async def repeat(self, ctx, *, content: str):
+        await ctx.reply(content)
 
 
 async def setup(bot):
